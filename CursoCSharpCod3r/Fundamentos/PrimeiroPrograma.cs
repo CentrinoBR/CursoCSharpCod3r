facf53f63c9224eafabfe4a4eed2c70cd7b33694
Centrino <railson@msn.com>
2018-08-24 15:25:20 -0300

    Alterando o método de entrada Main para outro arquivo .cs em subpasta, entendo a possiblidade de utilizar múltiplos "Main".

--- CursoCSharpCod3r/Fundamentos/PrimeiroPrograma.cs
+++ CursoCSharpCod3r/Fundamentos/PrimeiroPrograma.cs
@@ -1,10 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace CursoCSharpCod3r.Fundamentos
 {
     class PrimeiroPrograma
     {
+        static void Main()
+        {
+            System.Console.Write("Primeiro");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+        }
     }
 }
